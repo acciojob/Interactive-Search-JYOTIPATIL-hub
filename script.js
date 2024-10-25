4ee3f82e7a462506ad908a1bbf015d68415cb6b8
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-25 22:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function activateSearch() {
+    const searchDiv = document.querySelector(".search");
+    const searchInput = document.querySelector(".input");
+
+    // Add the active class to the search div and focus on the input
+    searchDiv.classList.add("active");
+    searchInput.focus();
+}
